f38873ab7763ca65d2acbd9bac75ae3ed765dd84
felix.d.tran@gmail.com <felixtran@Felixs-MacBook-Pro.local>
2017-03-30 10:44:20 -0700

    initialized server and webpack

---
+++ src/client/components/Test.jsx
@@ -0,0 +1,18 @@
+import React from 'react';
+
+class Test extends React.Component {
+  constructor(props) {
+    super(props);
+  }
+
+  render () {
+    return (
+      <h1>Benji</h1>
+    );
+  }
+};
+
+
+export default Test;
+//export a 
+//use to export specific properties not the Class
