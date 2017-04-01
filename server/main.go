64d25e490a36ff6bebacbed853cadd924bff6a41
Felix <felix.d.tran@gmail.com>
2017-03-31 19:30:35 -0700

    authentication implemented

--- server/main.go
+++ server/main.go
@@ -2,11 +2,19 @@ package main;
 
 import (
 	"net/http"
+	// "log"
+	// "encoding/json"
+	// "golang.org/x/crypto/bcrypt"
 )
+
 
 func main() {
 	test();
-	http.Handle("/", http.FileServer(http.Dir("../public/")));
-	http.Handle("/bundles/", http.StripPrefix("/bundles/", http.FileServer(http.Dir("../src/bundles/"))))
+	bundle := http.StripPrefix("/bundles/", http.FileServer(http.Dir("../src/bundles/")));
+	public := http.FileServer(http.Dir("../public/"))
+	http.Handle("/", public);
+	http.Handle("/bundles/", bundle);
+	http.HandleFunc("/api/signup", signUp);
+	http.HandleFunc("/api/login", logIn);
 	http.ListenAndServe(":8080", nil);
 }
