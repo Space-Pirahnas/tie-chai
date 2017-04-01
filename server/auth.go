5633d737969f7738dc211099b7a8955ef1d43dfd
Felix <felix.d.tran@gmail.com>
2017-04-01 14:23:44 -0700

    set up postgres and improved error handling

--- server/auth.go
+++ server/auth.go
@@ -9,30 +9,26 @@ import (
 )
 
 type user struct {
-	Username, Name, Password, Email string
+	Name, Password, Email, City string
 }
-
-type hashedUser struct {
-	Username, Name, Email string
-	Password []byte
-}
-
-var testDb = map[string]hashedUser{};
 
 func signUp (w http.ResponseWriter, req *http.Request) {
 	var u user;
+	decoder := json.NewDecoder(req.Body);
+	err := decoder.Decode(&u);
+	defer req.Body.Close();
 	if req.Method == http.MethodPost {
-		fu, found := testDb[u.Username];
-		if found {
-			log.Fatalln("username already taken");
+		var users Users;
+		db.Where(&Users{ Email: u.Email }).First(&users);
+		if len(users.Email) > 0 {
+			http.Error(w, "Email already taken", http.StatusBadRequest);
 		} else {
-			decoder := json.NewDecoder(req.Body);
-			err := decoder.Decode(&u);
-			defer req.Body.Close();
 			if err != nil { log.Println("user structure incorrect"); }
 			bp, err := bcrypt.GenerateFromPassword([]byte(u.Password), bcrypt.MinCost);
 			if err != nil { log.Println("hashing failed"); }
-			testDb[u.Username] = hashedUser{ u.Username, u.Name, u.Email, bp, }
+			var city Cities;
+			db.Where(&Cities{ City_Name: u.City }).First(&city);
+			db.Create(&Users{Name: u.Name, Email: u.Email, Password: bp, City_ID: city.ID });
 			w.Header().Set("Content-Type", "application/json");
 			r, _ := json.Marshal("successfully signed up");
 			log.Println("successfully signed up user");
@@ -50,14 +46,15 @@ func logIn (w http.ResponseWriter, req *http.Request) {
 		err := decoder.Decode(&u);
 		defer req.Body.Close();
 		if err != nil { log.Println("user structure incorrect"); }
-		fu, found := testDb[u.Username];
-		if found {
-			er := bcrypt.CompareHashAndPassword(fu.Password, []byte(u.Password));
+		var users Users;
+		db.Where(&Users{ Email: u.Email }).First(&users);	
+		if len(users.Email) > 0 {
+			er := bcrypt.CompareHashAndPassword(users.Password, []byte(u.Password));
 			if er != nil {
-				log.Println("password is incorrect");
+				http.Error(w, "password is incorrect", http.StatusBadRequest);
 			} else {
 				token := jwt.NewWithClaims(jwt.SigningMethodHS256, jwt.MapClaims{
-					"Username": u.Username,
+					"Email": u.Email,
 					"Time": time.Now(),
 				});
 				tokenString, _ := token.SignedString(secret);
@@ -67,7 +64,7 @@ func logIn (w http.ResponseWriter, req *http.Request) {
 				w.Write(r);
 			}
 		} else {
-			log.Println("user not found in db, please signup");
+			http.Error(w, "user not found in db, please signup", http.StatusFound);
 		}
 	} else {
 		log.Println("post method required");
