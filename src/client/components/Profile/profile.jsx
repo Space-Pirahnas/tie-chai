60e5f1f863ef30c376c040fdbc11b3b8458a7f76
Daryll <daryllcsh@gmail.com>
2017-04-04 20:52:10 -0700

    Add signin and signout buttons

--- src/client/components/Profile/profile.jsx
+++ src/client/components/Profile/profile.jsx
@@ -12,6 +12,12 @@ class Profile extends React.Component {
           <div className="name">Veer Gangwal</div>
           <div className="image"><img src={"styles/user.jpeg"} /></div>
         </div>
+        <div className="button-wrapper">
+          <a href="/#/auth/signin" className="Button">Sign In</a>
+        </div>
+        <div className="button-wrapper">
+          <a href="/#" className="Button">Sign Out</a>
+        </div>
       </div>
     );
   }
