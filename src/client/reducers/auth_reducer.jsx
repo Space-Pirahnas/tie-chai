8ac6979fad88bb51c93f84956221a72d141a1248
Yang Ruan <icelemo1989@hotmail.com>
2017-04-06 10:39:41 -0700

    Add check authorization function

--- src/client/reducers/auth_reducer.jsx
+++ src/client/reducers/auth_reducer.jsx
@@ -4,15 +4,20 @@ import {
   AUTH_ERROR,
 } from '../actions/types.jsx';
 
-export default function(state = {}, action) {
-  // switch(action.type) {
-  //   case AUTH_USER:
-  //     return { ...state, error: '', authenticated: true, username: action.payload };
-  //   case UNAUTH_USER:
-  //     return { ...state, authenticated: false, username: null };
-  //   case AUTH_ERROR:
-  //     return { ...state, error: action.payload };
-  // }
+const initialState = {
+  email: null,
+  authenticated: false
+}
+
+export default function(state = initialState, action) {
+  switch(action.type) {
+    case AUTH_USER:
+      return { ...state, error: '', authenticated: true, email: action.payload };
+    case UNAUTH_USER:
+      return { ...state, authenticated: false, email: null };
+    case AUTH_ERROR:
+      return { ...state, error: action.payload };
+  }
 
   return state;
 }
