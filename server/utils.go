05d7c9cb72adff86873105b85056369097f469c9
Felix <felix.d.tran@gmail.com>
2017-04-05 17:15:12 -0700

    removed comments

--- server/utils.go
+++ server/utils.go
@@ -7,7 +7,6 @@ import (
 )
 
 func successRequest(w http.ResponseWriter, res string, message string) {
-	// w.Header().Set("Content-Type", "application/json");
 	r, _ := json.Marshal(res);
 	log.Println(message);
 	w.Write(r);
