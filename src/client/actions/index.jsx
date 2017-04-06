7a5dd7eface5fd0f57f66e8600b9f039a30e98c9
Yang Ruan <icelemo1989@hotmail.com>
2017-04-05 19:27:19 -0700

    connect to backend of authorization

--- src/client/actions/index.jsx
+++ src/client/actions/index.jsx
@@ -1,6 +1,6 @@
 import axios from 'axios';
 import fetch from 'isomorphic-fetch';
-import { browserHistory } from 'react-router';
+import { hashHistory } from 'react-router';
 import { AUTH_USER, UNAUTH_USER, AUTH_ERROR } from './types.jsx';
 
 const axiosInstance = axios.create({
@@ -10,35 +10,47 @@ const axiosInstance = axios.create({
 export function signinUser({ email, password }) {
   console.log("Calling signinUser");
   return function (dispatch) {
-    axiosInstance.post('/api/login', {'Email': email, 'Password': password})
-    .then(response => {
-      console.log("successfully sign in an user and receive token as ", response.body);
-      dispatch({ type: AUTH_USER, payload: email });
-      // browserHistory.push('/home');
+    axiosInstance.post('/api/login', { 'Email': email, 'Password': password })
+      .then(response => {
+        console.log("successfully sign in an user and receive token as ", response.data);
+        dispatch({ type: AUTH_USER, payload: email });
+        hashHistory.push('/home');
+      })
+      .catch(error => {
+        console.error("fail to sign in an user with error ", error);
+        dispatch({ type: AUTH_ERROR, payload: error });
+      })
+  }
+}
+
+export function signupUser(signupObj) {
+  console.log("Calling signupUser", signupObj);
+  return function (dispatch) {
+    axiosInstance.post('/api/signup', signupObj)
+      .then(response => {
+        console.log("successfully sign up an user and receive token as ", response.data);
+        dispatch({ type: AUTH_USER, payload: signupObj.email });
+        hashHistory.push('/home');
+      })
+      .catch(error => {
+        console.error("fail to sign in an user with error ", error);
+        dispatch({ type: AUTH_ERROR, payload: error });
+      })
+  }
+}
+
+export function getUser(token) {
+  return function(dispatch) {
+    dispatch({type: AUTH_USER})
+    axios.get('/api/user', {
+      headers: {
+        authorization: token
+      }
     })
-    .catch(error => {
-      console.error("fail to sign in an user with error ", error);
-      dispatch({ type: AUTH_ERROR, payload: error });
+    .then(res => {
+      // const { email } = res.data
+      // dispatch({ type: AUTH_USER, payload: email });
+      // dispatch(getFavorites(username))
     })
   }
-  //   fetch("http://e5b70b93.ngrok.io/api/login", {
-  //     method: 'POST',
-  //     headers: {
-  //       'Accept': 'application/json',
-  //       'Content-Type': 'application/json',
-  //       'Access-Control-Allow-Origin': '*'
-  //     },
-  //     // mode: 'no-cors',
-  //     Body: {
-  //       'Email': email,
-  //       'Password': password
-  //     }
-  //   })
-  //   .then(response => {
-  //     console.log("successfully sign in an user and receive token as ", response.body);
-  //   })
-  //   .catch(err => {
-  //     console.error("fail to sign in an user with error ", err);
-  //   })
-  // }
 }
