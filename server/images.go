096b57984b03d94ce3645fa70051837527b8d491
felix.d.tran@gmail.com <felixtran@Felixs-MacBook-Pro.local>
2017-04-05 17:08:14 -0700

    added friends and fixed db bugs

--- server/images.go
+++ server/images.go
@@ -13,16 +13,23 @@ func handleImage(w http.ResponseWriter, req *http.Request) {
 	var img upload;
 	var u Users;
 	var i Image;
-	if req.Method == http.MethodPost {
+	if req.Method != http.MethodGet {
 		db.Where(&Users{Email: img.Email}).First(&u);
 		defer req.Body.Close();
 		json.NewDecoder(req.Body).Decode(&img);
-		updateImage(img, u, i);
-		successRequest(w, "success", "posted image url");
+		if req.Method == http.MethodPost {
+			updateImage(img, u, i);
+			successRequest(w, "success", "posted image url");
+		} else if req.Method == http.MethodDelete {
+			deleteImage(img, u);
+			successRequest(w, "success", "deleted image url");
+		}
 	} else if req.Method == http.MethodGet {
 		img.Email = req.URL.Query()["Email"][0];	
 		db.Where(&Users{Email: img.Email}).First(&u);
 		fetchImages(u, w);
+	} else if req.Method == http.MethodDelete {
+		img.E
 	}
 } 
 
@@ -39,7 +46,7 @@ func updateImage(img upload, u Users, i Image) {
 func fetchImages(u Users, w http.ResponseWriter) {
 	var i Image;
 	db.Where("id = ?", u.ImageID ).First(&i);
-	w.Header().Set("Content-Type", "application/json");
+	// w.Header().Set("Content-Type", "application/json");
 	r, _ := json.Marshal(u.Image);
 	w.Write(r);
 }
@@ -48,4 +55,14 @@ func getUserImage(u Users) string {
 	var img Image;
 	db.Where(&Image{ID: u.ImageID}).First(&img);
 	return img.ImageUrl;
+}
+
+func deleteImage(img upload, u Users) {
+	var image Image;
+	u.Image = "";
+	db.Save(&u);
+	db.Where(&Image{ImageUrl: img.ImageUrl}).Find(image);
+	if image.ImageUrl != "" {
+		db.Delete(&image);
+	}
 }
