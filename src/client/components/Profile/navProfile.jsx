98eb7e412690dac8283a353c8a72d1185c1142f4
Daryll <daryllcsh@gmail.com>
2017-04-08 16:39:51 -0700

    Update nav profile icon

--- src/client/components/Profile/navProfile.jsx
+++ src/client/components/Profile/navProfile.jsx
@@ -1,20 +1,32 @@
 import React from 'react';
+import { connect } from 'react-redux'
+import * as actions from '../../actions/index.jsx';
 
-class navProfile extends React.Component {
+class NavProfile extends React.Component {
   constructor(props) {
     super(props);
   }
 
   render() {
+    console.log('userinfo in navprofile: ', this.props.userInfo);
     return (
       <a href="/#/profile/:userid" className="UserProfile">
         <div className="User">
-          <div className="name">Veer Gangwal</div>
-          <div className="image"><img src={"styles/user.jpeg"} /></div>
+          <div className="name">{ this.props.userInfo.Name }</div>
+          <div className="image">
+            {
+              this.props.userInfo.Image ? <img src={ this.props.userInfo.Image } /> : null 
+            }
+          </div>
         </div>
       </a>
     );
   }
 };
 
-export default navProfile;
+function mapStateToProps(state) {
+  console.log('state in profile: ', state);
+  return { userInfo: state.userInfo.user }
+}
+
+export default connect(mapStateToProps, actions)(NavProfile);
