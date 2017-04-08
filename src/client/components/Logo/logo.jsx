d05af385f1ecc4d3dc56238393583c15dfd67228
Yang Ruan <summersocial1989@gmail.com>
2017-04-08 15:22:43 -0700

    show hide nav base on authenticated

--- src/client/components/Logo/logo.jsx
+++ src/client/components/Logo/logo.jsx
@@ -1,11 +1,16 @@
 import React from 'react';
+import { connect } from 'react-redux';
 // Logo
-const Logo = () => {
-		return (
-			<a href="/#/home" id="logo" className="Logo">
-				<img style={{"max-width": "100%", position: "relative" }} src={"styles/navLogo.png"} />
-			</a>
-		);
+const Logo = (props) => {
+  console.log("in the logo.jsx props", props);
+  return (
+    <a href={props.authenticated ? '/#/home' : '/'} id="logo" className="Logo">
+      <img style={{ "max-width": "100%", position: "relative" }} src={"styles/navLogo.png"} />
+    </a>
+  );
 };
 
-export default Logo;
+function mapStateToProps(state) {
+  return { authenticated: state.auth.authenticated };
+}
+export default connect(mapStateToProps)(Logo);
