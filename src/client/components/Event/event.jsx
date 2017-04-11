a1f42c6b85d7341188e29e23623468133a3466ef
Yang Ruan <summersocial1989@gmail.com>
2017-04-11 10:44:44 -0700

    track the chang on location text field

--- src/client/components/Event/event.jsx
+++ src/client/components/Event/event.jsx
@@ -6,15 +6,21 @@ import loadjs from 'loadjs';
 class CreateEvent extends React.Component {
   constructor(props) {
     super(props);
+    this.state = {
+      location : ''
+    }
     this.handleEventSubmit = this.handleEventSubmit.bind(this);
+    this.handleChangeAction = this.handleChangeAction.bind(this);
   }
 
-  componentWillMount() {
+  componentDidMount() {
     const url = "https://maps.googleapis.com/maps/api/js?key=" + GOOGLE_API + "&libraries=places";
     console.log("calling componentwillmount in event.jsx", url);
     loadjs(url, {
       success: () => {
         console.log("Successfully load google script");
+        const input = document.getElementById('google_auto')
+        console.log("input for autocomplete ", input);
       },
       error: (error) => {
         console.log("Fail to load google autocomplete api url");
@@ -23,15 +29,19 @@ class CreateEvent extends React.Component {
   
   }
 
-  handleEventSubmit(values) {
-    console.log("handleEventSubmit values is  ", values);
+  handleChangeAction(values) {
+    console.log("handleChangeAction Works ,", values);
+  }
+
+  handleEventSubmit(value) {
+    console.log("handleEventSubmit values ", value);
   }
 
   render() {
     return (
       <div style={{"marginTop": "10%"}}>
         <h1>CreateEvent List From CreateEvent.jsx</h1>
-        <EventForm onSubmit={this.handleEventSubmit} />
+        <EventForm onSubmit={this.handleEventSubmit} eventChange={this.handleChangeAction} />
       </div>
     );
   }
--- src/client/components/Event/eventformfields.jsx
+++ src/client/components/Event/eventformfields.jsx
@@ -4,15 +4,21 @@ export const EventField = {
   titleField: eventtitle => (
     <fieldset className="form-group">
       <label>Title: </label>
-      <input className="form-control" {...eventtitle.input} />
+      <input className="form-control" {...eventtitle.input} placeholder="Event Title" />
       {eventtitle.meta.touched && eventtitle.meta.error && <div className="error">{eventtitle.meta.error}</div>}
     </fieldset>
   ),
 
   locationField: location => (
-    <fieldset className="form-group">
+    <fieldset className="form-group" id="locattionField">
       <label>Location: </label>
-      <input className="form-control" {...location.input} />
+      <input id="google_auto" 
+      className="form-control" {...location.input} 
+      onChange={(e) => {
+        location.onChangeAction(location.input.value);
+        location.input.onChange(e);
+      }} 
+      placeholder="Enter meet address ..." />
       {location.meta.touched && location.meta.error && <div className="error">{location.meta.error}</div>}
     </fieldset>
   ),
