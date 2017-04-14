ce550a2f9f9c2659855881c80ead58cd756cc2a5
Daryll <daryllcsh@gmail.com>
2017-04-13 17:10:19 -0700

    Seperate components in profile

--- src/client/components/Profile/profile.jsx
+++ src/client/components/Profile/profile.jsx
@@ -39,38 +39,39 @@ class Profile extends React.Component {
         </div>
       )
       return (
-        <div className="ProfileContainer">
-          <div className="Profile">
-            <div className="ProfilePicture">
-              {
-                !this.state.edit ? this.props.target.Image ? <ProfilePic /> : <img className="profileImage" src={"./styles/noprofile.png"} /> : null
-              }
-              {this.state.edit && this.props.target.Email === this.props.user.Email ? <div className="profileImage"><UploadImage /></div> : null }
-            </div>
-            <div className="ProfileInfo">
-              {!this.state.edit  ? this.props.user.Email === this.props.target.Email ? <button className="Button" onClick={this.toggleEdit}>Edit Profile Picture</button> : null : <button className="Button" onClick={this.toggleEdit}>Cancel</button> }
-              <div className="ProfileRating">
-                <Rating value={this.props.target.Rating_Average} max={5} readOnly={true} />
+        <div className="background" style={{backgroundImage: "url(styles/retina_wood.png)"}}>
+          <div className="ProfileContainer">
+            <div className="Profile" style={{backgroundImage: "url(styles/creampaper.png)"}}>
+              <div className="ProfilePicture">
+                {
+                  !this.state.edit ? this.props.target.Image ? <ProfilePic /> : <img className="profileImage" src={"./styles/noprofile.png"} /> : null
+                }
+                {this.state.edit && this.props.target.Email === this.props.user.Email ? <div className="profileImage"><UploadImage /></div> : null }
               </div>
-              <div className="ProfileName">{this.props.target.Name}</div>
-              <div className="ProfileContactInfo">
-                {this.props.target.Profession} @ {this.props.target.Company}  -  {this.props.target.City}  -  {this.props.target.Email}
+              <div className="ProfileInfo">
+                {!this.state.edit  ? this.props.user.Email === this.props.target.Email ? <button className="Button" onClick={this.toggleEdit}>Edit Profile Picture</button> : null : <button className="Button" onClick={this.toggleEdit}>Cancel</button> }
+                <div className="ProfileRating">
+                  <Rating value={this.props.target.Rating_Average} max={5} readOnly={true} />
+                </div>
+                <div className="ProfileName">{this.props.target.Name}</div>
+                <div className="ProfileContactInfo">
+                  {this.props.target.Profession} @ {this.props.target.Company}  -  {this.props.target.City}  -  {this.props.target.Email}
+                </div>
+                <h2>Interests</h2>
+                <div className="ProfileInterests">
+                  { this.props.target ? this.props.target.Interests.split('-').map((interest,i) => <div className="interest Button" key={i}>{interest}</div>) : null}
+                </div>
+                <div className="ProfileBio">{this.props.target.Bio}</div>
+                <a href="/#/message" className="Button">Message</a>
               </div>
-              <h3>Interests</h3>
-              <div className="ProfileInterests">
-                { this.props.target ? this.props.target.Interests.split('-').map((interest,i) => <div className="interest Button" key={i}>{interest}</div>) : null}
+            </div>         
+            <div className="ProfileReview">
+              <h2 style={{"margin-top": "50px"}} >Reviews</h2>
+              <center><button className="Button" onClick={this.toggleReview} >Write A Review!</button></center>
+              {this.state.review ? <SubmitReview  /> : null }
+              <div className="Reviews">
+                {this.props.target.Reviews ? this.props.target.Reviews.map((review, i) => <Review key={i} review={review} />) : null}
               </div>
-              <div className="ProfileBio">{this.props.target.Bio}</div>
-              <a href="/#/message" className="Button">Message</a>
-            </div>
-          </div>
-          
-          <div className="ProfileReview">
-            <h2 style={{"margin-top": "50px"}} >Reviews</h2>
-            <center><button className="Button" onClick={this.toggleReview} >Write A Review!</button></center>
-            {this.state.review ? <SubmitReview  /> : null }
-            <div className="Reviews">
-              {this.props.target.Reviews ? this.props.target.Reviews.map((review, i) => <Review key={i} review={review} />) : null}
             </div>
           </div>
         </div>
