78122305a8aaef480134347959f9636305d4241c
Daryll <daryllcsh@gmail.com>
2017-04-15 16:43:03 -0700

    Add notification button to navbar

---
+++ src/client/components/Nav/notification.jsx
@@ -0,0 +1,29 @@
+import React from 'react';
+import { connect } from 'react-redux'
+import * as actions from '../../actions/index.jsx';
+import { Link, hashHistory } from 'react-router';
+import Badge from 'material-ui/Badge';
+import IconButton from 'material-ui/IconButton';
+import NotificationsIcon from 'material-ui/svg-icons/social/notifications';
+
+class Notification extends React.Component {
+  constructor(props) {
+    super(props);
+  }
+
+  render() {
+    return (
+      <Badge badgeContent={4} primary={true} badgeStyle={{top: 12, right: 12}} style={{ padding: "15px 8px 0px 0px" }}>
+        <IconButton tooltip="Notifications">
+          <NotificationsIcon />
+        </IconButton>
+      </Badge>
+    );
+  }
+};
+
+function mapStateToProps(state) {
+  return { userInfo: state.userInfo.user }
+}
+
+export default connect(mapStateToProps, actions)(Notification);
