f36b675b30fc31304f9d37a3864cdde7ed83599b
Daryll <daryllcsh@gmail.com>
2017-04-15 17:08:25 -0700

    Reset notification on click

--- src/client/components/Nav/notification.jsx
+++ src/client/components/Nav/notification.jsx
@@ -9,11 +9,26 @@ import NotificationsIcon from 'material-ui/svg-icons/social/notifications';
 class Notification extends React.Component {
   constructor(props) {
     super(props);
+
+    this.state = {
+      notifications: 4
+    }
+    this.onClick = this.onClick.bind(this);
+  }
+
+  onClick() {
+    hashHistory.push('/');
+    setTimeout(() => {
+      hashHistory.push(`/#/home`)
+    });
+    this.setState({
+      notifications: 0
+    })
   }
 
   render() {
     return (
-      <Badge badgeContent={4} primary={true} badgeStyle={{top: 12, right: 12}} style={{ padding: "15px 8px 0px 0px" }}>
+      <Badge onClick={ this.onClick } badgeContent={ this.state.notifications } primary={true} badgeStyle={{top: 12, right: 12}} style={{ padding: "15px 8px 0px 0px" }}>
         <IconButton tooltip="Notifications">
           <NotificationsIcon />
         </IconButton>
