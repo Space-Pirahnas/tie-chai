cfdf54b8850d1ccb5228ba2d6b83f6ac26e0f6df
Daryll <daryllcsh@gmail.com>
2017-04-17 20:06:46 -0700

    Add carousel to saved list

--- src/client/components/Save/save.jsx
+++ src/client/components/Save/save.jsx
@@ -3,6 +3,7 @@ import { connect } from 'react-redux';
 import * as actions from '../../actions/saves.jsx';
 import SavedUser from './saveduser.jsx';
 import { axiosInstance } from '../../actions/index.jsx';
+import { AutoRotatingCarousel } from 'material-auto-rotating-carousel';
 
 class Save extends Component {
   constructor(props) {
@@ -14,14 +15,23 @@ class Save extends Component {
   }
 
   render() {
-    return (
-      <div className="save">
-        {this.props.savedUsers ? this.props.savedUsers.map((save, i) => <SavedUser key={i} save={save} />) : null}
-      </div>
-    )
+    if (this.props && this.props.savedUsers.length) {
+      return (
+        <AutoRotatingCarousel label="Get started" open >
+          {this.props.savedUsers.map((save, i) =>
+            <SavedUser save={save} key={i} />
+            )
+          }
+        </AutoRotatingCarousel>
+      )
+    } else {
+      return (<div>loading</div>);
+    }
   }
-
 }
+
+{/*{this.props.savedUsers ? this.props.savedUsers.map((save, i) => <SavedUser key={i} save={save} />) : null}*/ }
+
 
 function mapStateToProps(state) {
   return {
--- src/client/components/Save/saveduser.jsx
+++ src/client/components/Save/saveduser.jsx
@@ -1,19 +1,44 @@
 import React, { Component } from 'react';
+import { Slide } from 'material-auto-rotating-carousel';
+import { green400, green600, blue400, blue600, red400, red600 } from 'material-ui/styles/colors';
 
 export default class SavedUser extends Component {
   constructor(props) {
     super(props)
+
   }
 
   render() {
     return (
-      <div>
-        <div>{this.props.save.Name}</div>
-        <div>{this.props.save.Email}</div>
-        <div>{this.props.save.Interests.split('-').map((interest, i) => <div key={i}>{interest}</div> )}</div>
-        <div>{this.props.save.Company}</div>
-        <div>{this.props.save.Bio}</div>
-      </div>
+      <Slide
+        media={<img src={ url(`${this.props.save.Image}`) } />}
+        mediaBackgroundStyle={{ backgroundColor: red400 }}
+        contentStyle={{ backgroundColor: red600 }}
+        title={ this.props.save.Name }
+        subtitle={ this.props.save.Bio }
+      />
     )
   }
 }
+
+{/*const SavedInfo = () => {
+  return (
+    <div className="contactContainer">
+      <div>
+         {`${ this.props.save.Profession } @ ${ this.props.save.Company }`}
+      </div>
+      <div className="contactBio">
+        { `${(this.props.save.Bio).substring(0, 300)}...` }     
+      </div>
+      <div className="contactInterests">
+        { this.props.save.Interests.split('-').map((interest, i) =>
+          <div className="profileInterest" key={i}>{interest}</div>
+        )}
+      </div>
+      <div className="contactButtons">
+        <a style={{ margin: "0 20px" }} href="/#/message" className="Button">Message</a>
+        <a style={{ margin: "0 20px" }} href={`/#/profile/${ this.props.save.Email }`} className="Button">Profile</a>
+      </div>
+    </div>
+  )
+}*/}
