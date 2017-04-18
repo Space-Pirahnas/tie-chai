04d4b8faa84759c0196bdf976149b75ed3db7f7d
Daryll <daryllcsh@gmail.com>
2017-04-17 20:20:32 -0700

    Update carousel

--- src/client/components/Save/saveduser.jsx
+++ src/client/components/Save/saveduser.jsx
@@ -9,36 +9,35 @@ export default class SavedUser extends Component {
   }
 
   render() {
+    const SavedInfo = () => {
+      return (
+        <div className="contactContainer">
+          <div className="contactBio">
+            { `${(this.props.save.Bio).substring(0, 300)}...` }     
+          </div>
+          <div className="contactInterests">
+            { this.props.save.Interests.split('-').map((interest, i) =>
+              <div className="profileInterest" key={i}>{interest}</div>
+            )}
+          </div>
+          <div className="contactButtons">
+            <a style={{ margin: "0 20px" }} href="/#/message" className="Button">Message</a>
+            <a style={{ margin: "0 20px" }} href={`/#/profile/${ this.props.save.Email }`} className="Button">Profile</a>
+          </div>
+        </div>
+      )
+    }
     return (
-      <Slide
-        media={<img src={ url(`${this.props.save.Image}`) } />}
-        mediaBackgroundStyle={{ backgroundColor: red400 }}
-        contentStyle={{ backgroundColor: red600 }}
-        title={ this.props.save.Name }
-        subtitle={ this.props.save.Bio }
-      />
+      <div>
+        <Slide
+          media={<img src={ this.props.save.Image } />}
+          mediaBackgroundStyle={{ backgroundColor: red400 }}
+          contentStyle={{ backgroundColor: red600 }}
+          title={ this.props.save.Name }
+          subtitle={`${ this.props.save.Profession } @ ${ this.props.save.Company }`}
+        />
+        <SavedInfo />
+      </div>
     )
   }
 }
-
-{/*const SavedInfo = () => {
-  return (
-    <div className="contactContainer">
-      <div>
-         {`${ this.props.save.Profession } @ ${ this.props.save.Company }`}
-      </div>
-      <div className="contactBio">
-        { `${(this.props.save.Bio).substring(0, 300)}...` }     
-      </div>
-      <div className="contactInterests">
-        { this.props.save.Interests.split('-').map((interest, i) =>
-          <div className="profileInterest" key={i}>{interest}</div>
-        )}
-      </div>
-      <div className="contactButtons">
-        <a style={{ margin: "0 20px" }} href="/#/message" className="Button">Message</a>
-        <a style={{ margin: "0 20px" }} href={`/#/profile/${ this.props.save.Email }`} className="Button">Profile</a>
-      </div>
-    </div>
-  )
-}*/}
