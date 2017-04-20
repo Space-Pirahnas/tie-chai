c160bb3998e726d8e2f664bff9c9dd72bc8507d3
Yang Ruan <summersocial1989@gmail.com>
2017-04-20 16:51:38 -0700

    add location and time

--- src/client/components/EventView/eventView.jsx
+++ src/client/components/EventView/eventView.jsx
@@ -5,7 +5,7 @@ import FlatButton from 'material-ui/FlatButton';
 import { hashHistory } from 'react-router';
 import Comment from './Comments/comment.jsx';
 import SubmitComment from './Comments/submitComment.jsx';
-import TimeComponent from './timeComponent.jsx';
+import TimeLocation from './timeLocation.jsx';
 
 class EventView extends Component {
 
@@ -21,50 +21,51 @@ class EventView extends Component {
     this.deleteEvent = this.deleteEvent.bind(this);
   }
 
-  toggleComment(){
+  toggleComment() {
     this.setState({
       comment: !this.state.comment
     });
   }
 
-  RSVP(){
+  RSVP() {
     this.props.rsvpForEvent(this.props.user.Email, this.props.params.eventID);
   }
 
-  deleteRSVP(){
+  deleteRSVP() {
     this.props.deleteRSVP(this.props.user.Email, this.props.params.eventID);
   }
 
-  routeToAttendee(attendee){
+  routeToAttendee(attendee) {
     hashHistory.push(`/profile/${attendee.Email}`);
   }
 
-  deleteEvent(){
+  deleteEvent() {
     this.props.deleteEvent(this.props.user.Email, this.props.params.eventID);
   }
 
 
-  render(){
+  render() {
     if (this.props.event.target) {
       console.log("In the eventView the target evetn is ", this.props.event.target);
-      const { Image, Email, Attendees, Comments, Title, Date } = this.props.event.target;
-      return(
-        <div>
+      const { Image, Email, Attendees,
+        Comments, Title, Date, Business, Location } = this.props.event.target;
+      return (
+        <div style={{"marginTop": "15%"}}>
           <div>
             <h1 className="title-display">{Title}</h1>
-            <TimeComponent datetime={Date} />
+            <TimeLocation datetime={Date} business={Business} location={Location} />
             <div>
               <img src={Image} className="event_page_image" />
             </div>
             <div className="attendees">
               {this.props.user.Email === Email ? <FlatButton label="delete event" secondary={true} onClick={this.deleteEvent} /> : null}
-              <h1>{Attendees ?  Attendees.length : 0} - Attendees</h1>
+              <h1>{Attendees ? Attendees.length : 0} - Attendees</h1>
               {Attendees ? Attendees.map((attendee, i) => <FlatButton onClick={this.routeToAttendee.bind(this, attendee)} key={i} label={attendee.Name} />) : null}
               {this.props.event.rsvp ? <FlatButton label="rsvp" onClick={this.RSVP} primary={true} /> : <FlatButton label="cancel rsvp" onClick={this.deleteRSVP} secondary={true} />}
             </div>
           </div>
           <FlatButton label="Write A Comment!" onClick={this.toggleComment} />
-          {this.state.comment ? <SubmitComment toggleComment={this.toggleComment} eventKey={this.props.params.eventID} /> : null } 
+          {this.state.comment ? <SubmitComment toggleComment={this.toggleComment} eventKey={this.props.params.eventID} /> : null}
           {Comments ? Comments.map((c, i) => <Comment comment={c} key={i} />) : null}
         </div>
       )
---
+++ src/client/components/EventView/timeLocation.jsx
@@ -0,0 +1,30 @@
+import React from 'react';
+
+const TimeLocation = (props) => {
+  const { business, location } = props;
+  const datetime = props.datetime.split(' at ');
+  const date = datetime[0], time = datetime[1];
+  console.log('in time component date and time', date, time);
+  return (
+    <div>
+      <ul>
+        <li className="eventList">
+          <div className="iconClass"><i className="fa fa-clock-o fa-lg" /></div>
+          <ul>
+            <li><h3 className="subTitle-display">{date}</h3></li>
+            <li><h4>{time}</h4></li>
+          </ul>
+        </li>
+        <li className="eventList">
+          <div className="iconClass"><i className="fa fa-map-marker fa-lgs" /></div>
+          <ul>
+            <li><h3 className="subTitle-display">{business}</h3></li>
+            <li><h4>{location}</h4></li>
+          </ul>
+        </li>
+      </ul>
+    </div>
+  );
+};
+
+export default TimeLocation;
