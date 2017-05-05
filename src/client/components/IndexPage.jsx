ad503918d8e3b595f47533877a6eb1cab4759d00
Daryll <daryllcsh@gmail.com>
2017-05-04 17:50:56 -0700

    Input data into piechart

--- src/client/components/IndexPage.jsx
+++ src/client/components/IndexPage.jsx
@@ -11,9 +11,14 @@ class IndexPage extends Component {
   render() {
 
     let data = {
-      label: 'somethingA',
-      values: [{ x: 'SomethingA', y: 10 }, { x: 'SomethingB', y: 4 }, { x: 'SomethingC', y: 3 }]
-    };
+      label: 'Interests',
+      values: this.props.interests.map( (i) => {
+        return {
+          x: i.Interest_Name,
+          y: 10
+        }
+      })
+    }
 
     let sort = null;
     return (
